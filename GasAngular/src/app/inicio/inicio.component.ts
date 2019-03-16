87b1d6c533b952ce58e150a761b610257d470418
Mario-Cosan <mariocosan@gmail.com>
2019-03-16 20:06:40 +0100

    Prueba completa  => OK

--- GasAngular/src/app/inicio/inicio.component.ts
+++ GasAngular/src/app/inicio/inicio.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-
+contenedor: any
   constructor() { }
 
   ngOnInit() {
   }
+
+  
 
 }
